0ff83dab38e36b2d71d84a5011018e8d6dbf5ddb
Druce37 <aindras.guillaume@proton.me>
2026-03-19 14:35:09 -0600

    MOUSEY-QA

---
+++ bottle_app.py
@@ -0,0 +1,328 @@
+#http://bit.ly/mousey-qa
+# A very simple Bottle Hello World app for you to get started with...
+from bottle import default_app, route
+
+@route('/')
+def hello_world():
+    return """
+
+    <DOCTYPE! html>
+
+    <head>
+
+        <style>
+
+            body {
+
+                background-color: #000000;
+
+                color: #FFFFFF;
+
+                font-size: 2.62em;
+
+                }
+
+            a   {
+
+                color: #00FFFF;
+
+            }
+
+            p , h1 , h2 , h3 , h4 , h5 , h6    {
+
+                color: #1aff1a;
+
+                }
+
+            p.ylloh , h1.ylloh , h2.ylloh , h3.ylloh , h4.ylloh , h5.ylloh , h6.ylloh {
+
+                color: yellow;
+
+                }
+
+            div.bluu-line-thing {
+
+                border-bottom: 3px dotted blue;
+
+            }
+
+            div.red-line-thing {
+
+                border-bottom: 3px dotted red;
+            }
+
+        </style>
+
+    </head>
+
+    <body>
+
+        <h6> { <i> work-in-progress ... </i> } </h6>
+
+        <h3> introducing ... </i> </h3>
+
+        <h1 class="ylloh"> <b> Mousey-Qa & Pals </b> </h1>
+
+        <h2 class="ylloh"> </i> at the School for All Ages </i> </h2>
+
+        <h4> most recent episode ( more below ) : </h4>
+
+        <p> <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2285714036&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/drucey-710670561" title="Drucey" target="_blank" style="color: #cccccc; text-decoration: none;">Drucey</a> · <a href="https://soundcloud.com/drucey-710670561/priscilla-weighs-in-on" title="Priscilla Weighs In On Emotionality Towards End - March 17, 2026 at 3:16 PM" target="_blank" style="color: #cccccc; text-decoration: none;">Priscilla Weighs In On Emotionality Towards End - March 17, 2026 at 3:16 PM</a></div> </p>
+
+        <p> <i> mere commentary ... </i> </p>
+
+        <p> spakethed : major emotional responses / emotions : </p>
+
+        <ul>
+
+            <li>
+
+                <p> <i> glad </i> - happy | joyous | pleased | satisfied | contented </p>
+
+            </li>
+
+            <li>
+
+                <p> <i> rad </i> - excited | ecstatic | delighted | amazed | wowed </p>
+
+            </li>
+
+            <li>
+
+                <p> <i> badly </i> - regretful | despondent | "upset" | ashamed | guilty </p>
+
+            </li>
+
+            <li>
+
+                <p> <i> sadly </i> - mournful | grieving | lamenting | "upset" | dejected </p>
+
+            </li>
+
+            <li>
+
+                <p> <i> madly </i> - angry | frustrated | bothered | "spaced out" | "upset" </p>
+
+            </li>
+
+        </ul>
+
+        <p> also : scared , anxious , fearful , afraid , worried </p>
+
+        <p> ... and revulsion , discomfort , nervousness , hurt , <i> etcetera </i> </p>
+
+        <div class="bluu-line-thing"></div>
+
+        <p> for questions , comments , compliments , collaboration and more ... </p>
+
+        <p> ... you may send a message to "Druce" : <a href="mailto:aindras.guillaume@proton.me"> aindras.guillaume@proton.me </a>
+
+        <p> <i> this website is <a href="collinsdictionary.com/dictionary/french-english/gratuit"> gratuit </a> from Aware Advisory Services </i> </p>
+
+        <div class="bluu-line-thing"></div>
+
+        <p> we <a href="https://www.merriam-webster.com/dictionary/noob"> noobies </a> at the School for All Ages ... </p>
+
+        <p> ... doth profess surely that some true thaengzs ... </p>
+
+        <p> ... oughta be known certainly , better , by all ... </p>
+
+        <p> ... such as the main categories of love ... </p>
+
+        <p> ... what comprises compassionate communication and other love-ways ... </p>
+
+        <p> ... and main aspects of enjoyable , sustainable living : </p>
+
+        <div class="red-line-thing"></div>
+
+        <h3 class="ylloh"> discerningly , the five Ancient Greek Love Types : </h3>
+
+        <p> <i> agape </i> - enlightenedly altruistic love for approximately everyone </p>
+
+        <p> <i> storje </i> - dedicated , devoted love with specific application </p>
+
+        <p> <i> philautia </i> - s'<a href="https://en.wikipedia.org/wiki/Elf_(film)">elf</a> love , best kept in moderation </p>
+
+        <p> <i> filea </i> - the love of chosen friends , pals </p>
+
+        <p> <i> ludus </i> - " gettin' crazy " , playfulness , child-like spirit </p>
+
+        <h3 class="ylloh"> ( the ) main aspects of life ( with contributions ) : </h3>
+
+        <h4> <u> trust & faith </u> </h4>
+
+        <p> fundamentally , this must include Yeshu of Ga </p>
+
+        <p> ( heretofore more commonly : " Christ Immanuel of <a href="https://en.wikipedia.org/wiki/Names_of_God"> God " </a> )
+
+        <p> ( and in fairness : <a href="https://en.wikipedia.org/wiki/Namaste"> Namaste </a> / <a href="https://en.wikipedia.org/wiki/Takbir"> Takbir! </a> / <a href="https://en.wikipedia.org/wiki/Shalom"> Shalom </a> / <i> etcetera </i> ) </p>
+
+        <p> in practice , whether prayer , contemplation , and-or meditation ... </p>
+
+        <p> ... if including and heeding Him ... 'tis <a href="https://en.wikipedia.org/wiki/Legit"> "legit" </a> </p>
+
+        <h4> <u> teamwork & friendship </u> </h4>
+
+        <p> agreements should be based upon veritable concurrence(s) </p>
+
+        <p> teamwork quite must include Yeshu for best ... </p>
+
+        <p> ... best results , more safe fun , joys , goodnesses ! </p>
+
+        <h4> <u> "shape" & fitness </u> </h4>
+
+        <p> <a href="https://www.spanishdict.com/translate/exercise"> por ejercisio </a> : " fun zone " ( relaxedly with eustress* ) </p>
+
+        <p> nutrition : all-natural , prayerfully smelled , chewed , drank </p>
+
+        <p> * beneficial stress from team efforts with Yeshu </p>
+
+        <h4> <u> contribution & fructivity </u> </h4>
+
+        <p> prayerful progress has proven itself repeatedly recently </p>
+
+        <p> metrics may make for easier remembrance , decisions </p>
+
+        <h4> <u> play & fun </u> </h4>
+
+        <p> recreation , wild wondering , safe adventuring ... </p>
+
+        <p> ... such becomes necessitated for continued goodness , love </p>
+
+        <h3 class="ylloh"> <a href="https://en.wikipedia.org/wiki/The_Five_Love_Languages"> " the Five Love Languages " </a> ( however , love-ways ) : </h3>
+
+        <p> <b> acts of service </b> - getting garlic for your <a href="https://youtu.be/oKv4a10USpw"> roommate </a> </p>
+
+        <p> <b> quality time </b> - such as with <a href="https://youtu.be/KydI7KF3YkM"> koala youth </a> </p>
+
+        <p> <b> words of praise </b> - <a href="https://youtu.be/MlBmnFxCYdo"> <i> like this </i> </a> #purityPals </p>
+
+        <p> <b> gifting & receiving </b> - <a href="https://youtu.be/CdJWaEZundk"> like couple boba keychains </a> </p>
+
+        <p> <b> nice intimacy </b> - <a href="https://youtu.be/t52DEpkNMGg"> similar to these hugs </a> #LoveWins #R-E-S-P-E-C-T </p>
+
+        <h3 class="ylloh"> compassionate communication ( ' Team-Talk ' | <a href="https://youtu.be/NYkgbrZSAY0?si=TUR_y0sNdQ_Zyesn"> "NVC" </a> ) : </h3>
+
+        <p> <i> 'onistad' honesty </i> - humbly conveyed experiential truths , views </p>
+
+            <ul>
+
+                <li>
+
+                    <p> " these boogers appear greenish to me " </p>
+
+                </li>
+
+                <li>
+
+                    <p> " i acknowledge that " </p>
+
+                </li>
+
+            </ul>
+
+        <p> <i> felt feelings </i> - actually ours ( glad , rad , "upset" ) </p>
+
+            <ul>
+
+                <li>
+
+                    <p> " i feel so bad about the Chipmunks " </p>
+
+                </li>
+
+                <li>
+
+                    <p> " what ? are you okay ? " </p>
+
+                </li>
+
+            </ul>
+
+        <p> <i> yummy yearnings </i> - needs / desires Yeshu is okay about </p>
+
+            <ul>
+
+                <li>
+
+                    <p> " i want nachos " </p>
+
+                </li>
+
+                <li>
+
+                    <p> " okay but not these " </p>
+
+                </li>
+
+            </ul>
+
+        <p> <i> respectful requests </i> - nobly asking > merely demanding </p>
+
+            <ul>
+
+                <li>
+
+                    <p> " turn it up please , it's Aretha singing " </p>
+
+                </li>
+
+                <li>
+
+                    <p> " R-E-S-P-E-C-T [...] " </p>
+
+                </li>
+
+            </ul>
+
+        <p> <i> cheerful chat </i> - for reprieve from above , fun </p>
+
+            <ul>
+
+                <li>
+
+                    <p> " howdy y'all ! " </p>
+
+                </li>
+
+                <li>
+
+                    <p> " hey ! how's your mamma ? " </p>
+
+                </li>
+
+            </ul>
+
+        <h2 class="ylloh"> the five Rs : </h2>
+
+        <p> Refraining from waste , negligence , harm </p>
+
+        <p> Reusing rather than only and merely disposing </p>
+
+        <p> Replenishing natural cycles ( flora , fauna , funga ) </p>
+
+        <p> "Ratcheting" - doing purely positive things daily </p>
+
+        <p> Renewing our minds , emboldening our spirits ... </p>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/OiYjTb3opAA?si=8t9EvFTvZAhfPOPd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+        <br>
+
+        <div class="line-thing"></div>
+
+        <p> for questions , comments , compliments , collaboration and more ... </p>
+
+        <p> ... you may send a message to "Druce" : <a href="mailto:aindras.guillaume@proton.me"> aindras.guillaume@proton.me </a>
+
+        <p> <i> this website is <a href="collinsdictionary.com/dictionary/french-english/gratuit"> gratuit </a> from Aware Advisory Services </i> </p>
+
+    </body>
+
+    </html>
+
+    """
+
+application = default_app()
+
+#http://bit.ly/mousey-qa
